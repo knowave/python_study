ab7597be2769d8136bafc549382d3929b18af1c3
knowave <kwaktaemin98@gmail.com>
2024-07-31 17:46:08 +0900

    urls에 blog 추가

--- web_study/main/urls.py
+++ web_study/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import *
 
-from . import views
+app_name='main'
 
-
-urlpatterns = [
-    path('', views.index, name='index'),
+urlpatterns=[
+    path('',index),
+    path('blog/',blog),
 ]
